1f140c6bfa12e0524f1d265cef1325b9aaa5f47a
joaoneves13 <13joaoneves@gmail.com>
2019-11-16 20:37:27 +0100

    Returns an empty list of items

--- src/shoppingCart.js
+++ src/shoppingCart.js
@@ -2,6 +2,12 @@ class ShoppingCart {
  constructor() {
       this.items = []
 }
+
+// Returns an empty list of items
+getItems() {
+    return this.items
+   }
 }
+
 
 module.exports = ShoppingCart
